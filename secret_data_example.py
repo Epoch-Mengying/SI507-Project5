e0703db9ccde6c7b34e50a92610425017e476cc8
Mengying Zhang <zhangmen@umich.edu>
2017-11-12 15:28:37 -0500

    added secret_data_example.py

---
+++ secret_data_example.py
@@ -0,0 +1,7 @@
+# This file stores client key(OAtuh consumer key) and client secret(OAuth consumer secret) for your application. 
+# Add your clinet key and client secret here and rename the file into secret_data.py
+client_key = ""
+client_secret = ""
+
+
+# Get yours from Tumblr API application registration here: https://www.tumblr.com/oauth/apps
