0c07e853940cb18f593cb23879a09c2219a7f3ef
Mengying Zhang <zhangmen@umich.edu>
2017-11-12 15:21:05 -0500

    Deleted secret_data.py

--- secret_data.py
+++
@@ -1,7 +0,0 @@
-# This file stores client key(OAtuh consumer key) and client secret(OAuth consumer secret) for my application MCracker. 
-
-client_key = "gdzo9C2PN4z0ulfot6Kxo2a7SzzhhTKoC1jDQxnMKUgQixvy7e"
-client_secret = "ncT5kdH8NmBkRuBVyxB9e6YcDaB5bLrXQGWzScCHTE3yeySvDf"
-
-
-# Get yours from Tumblr API application registration here: https://www.tumblr.com/oauth/apps
